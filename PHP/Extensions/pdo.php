2b60c1801702b0bf1501a877b91a77d7e25b6415
zhengchenping <zhengchenping@100tal.com>
2018-03-29 15:24:37 +0800

    +

--- PHP/Extensions/pdo.php
+++ PHP/Extensions/pdo.php
@@ -8,7 +8,7 @@ class PdoConnection
     {
         $host = $params['host'] ?? '127.0.0.1';
         $port = $params['port'] ?? '3306';
-        $dbname = $params['dbname'] ?? '';
+        $dbname = $params['database'] ?? '';
         $username = $params['username'] ?? '';
         $password = $params['password'] ?? '';
         $persistent = $params['persistent'] ?? true;
@@ -36,14 +36,12 @@ class PdoConf
 {
     private static  $connection = [
         'mysql' => [
-            'host' => '127.0.0.1',
+            'host' => '123.207.116.147',
             'port' => '3306',
-            'database' => '',
-            'username' => '',
-            'password' => '',
-            'persistent' => '',
-            'charset' => '',
-            'collation' => '',
+            'database' => 'azk_exam',
+            'username' => 'azk_project',
+            'password' => 'Puv3LKl3daloZ',
+            'persistent' => true
         ],
     ];
 
@@ -61,18 +59,139 @@ class PdoConf
 //Pdo实体方法
 class PdoEntity
 {
-    private $dbh = null;
+    private $dbh;
+
+    private $sth;
+
+    private $sql;
+
+    private $params;
+
+
 
     public function __construct($dbh)
     {
         $this->dbh = $dbh;
     }
 
-    public function select()
+    public function select($sql, $params)
+    {
+        $this->sql = $sql;
+        $this->params = $params;
+
+        if($this->execute()) {
+            return $this->sth->fetchAll();
+        }
+
+        return false;
+    }
+
+    public function insert($sql, $params)
+    {
+        $this->sql = $sql;
+        $this->params = $params;
+        return $this->execute();
+    }
+
+    public function update($sql, $params)
+    {
+        $this->sql = $sql;
+        $this->params = $params;
+        return $this->execute();
+    }
+
+    public function delete($sql, $params)
+    {
+        $this->sql = $sql;
+        $this->params = $params;
+        return $this->execute();
+    }
+
+
+    private function execute()
+    {
+        $this->validSql();
+        $this->validParams();
+
+        $this->sth = $this->dbh->prepare($this->sql);
+        foreach ($this->params as $key => $val) {
+
+            $param_type = PDO::PARAM_STR;
+            if(is_int($val)) {
+                $param_type = PDO::PARAM_INT;
+            }
+            else if(is_string($val)) {
+                $param_type = PDO::PARAM_STR;
+            }
+            else if(is_null($val)) {
+                $param_type = PDO::PARAM_NULL;
+            }
+            else if(is_bool($val)) {
+                $param_type = PDO::PARAM_BOOL;
+            }
+
+            $this->sth->bindValue($key, $val, $param_type);
+        }
+
+        $this->sth->execute();
+        if($this->sth->errorCode()  === '00000') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public function debugSql()
     {
 
     }
 
+    /**
+     * sql语句检查处理
+     */
+    private function validSql()
+    {
+
+    }
+
+    /**
+     * sql参数绑定检查处理
+     */
+    private function validParams()
+    {
+
+    }
+
+
+    /**
+     * 错误码
+     * @return string
+     */
+    public function errorCode()
+    {
+        if($this->sth) {
+            return $this->sth->errorCode();
+        }
+        else {
+            return '';
+        }
+    }
+
+    /**
+     * 错误信息
+     * @return string
+     */
+    public function errorInfo()
+    {
+        if($this->sth) {
+            return $this->sth->errorInfo();
+        }
+        else {
+            return '';
+        }
+    }
 }
 
 
@@ -85,13 +204,14 @@ class DB
 
     public static function __callStatic($name, $arguments)
     {
+
         if(!self::$pdoList[self::$defaultName]) {
             $conf = PdoConf::get(self::$defaultName);
             $dbh = PdoConnection::get($conf);
             self::$pdoList[self::$defaultName] = new PdoEntity($dbh);
         }
 
-        self::$pdoList[self::$defaultName]->{$name}($arguments[0], $arguments[1]);
+        return self::$pdoList[self::$defaultName]->{$name}($arguments[0], $arguments[1]);
     }
 
     public static function connection($name)
@@ -106,28 +226,28 @@ class DB
 }
 
 
-
-
+$res = DB::select('SELECT * FROM student WHERE studentId<:id',[':id'=>10]);
+var_dump($res);
 
 //$sql $params
-DB::select();
-DB::selectFetchOne();
-DB::selectFetchRow();
-
-DB::insert();
-DB::insertGetId();
-
-DB::update();
-DB::updateGetRow();
-
-DB::delete();
-DB::deleteGetRow();
-
-DB::beginTransaction();
-DB::commit();
-DB::rollBack();
-
-DB::connection('test')->select();
+//DB::select();
+//DB::selectFetchOne();
+//DB::selectFetchRow();
+//
+//DB::insert();
+//DB::insertGetId();
+//
+//DB::update();
+//DB::updateGetRow();
+//
+//DB::delete();
+//DB::deleteGetRow();
+//
+//DB::beginTransaction();
+//DB::commit();
+//DB::rollBack();
+//
+//DB::connection('test')->select();
 
 //查询构造器
 
